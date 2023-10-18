8da7dab1e8bd4640e87387e760fc6eacd4c13338
KukkalaSowmya <145061290+KukkalaSowmya@users.noreply.github.com>
2023-10-18 23:53:08 +0530

    Add files via upload

---
+++ SingleArray.java
@@ -0,0 +1,12 @@
+public class SingleArray{
+  public static void main(String[] args){
+    //create an array
+    int[] age = {12,3,7,5};
+    //access each array elements
+    System.out.println("Accessing Elents of array: ");
+    System.out.println("First Element: " + age[0]);
+    System.out.println("Second Element: " + age[1]);
+    System.out.println("Thirdd Element: " + age[2]);
+    System.out.println("Fourth Element: " + age[3]);
+      }
+}
